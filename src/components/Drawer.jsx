81ac3ff1d7cff9f07c9f9db495ec41a7a4021c3c
Dany0309 <142040968+Dany0309@users.noreply.github.com>
2023-12-10 13:07:28 +0500

    Урок 2
    Сделали компоненты, сделали overlay, сверстали корзину товаров

---
+++ src/components/Drawer.jsx
@@ -0,0 +1,74 @@
+const Drawer = () => {
+    return ( 
+        <div style={{display:'none'}} className="overlay">
+        <div className="drawer">
+          <h2 className="mb-20 d-flex justify-between cu-p">Корзина<img
+                  className="ml-40 removeBtn"
+                  src="/img/btn-remove.svg"
+                  alt="Remove"
+                /></h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <img
+                className="mr-20"
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="mr-20 d-flex justify-between">
+                <div className="box">
+                  <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                  <b>12 999 руб.</b>
+                </div>
+                <img
+                  className="ml-40 removeBtn"
+                  src="/img/btn-remove.svg"
+                  alt="Remove"
+                />
+              </div>
+            </div>
+            <div className="cartItem d-flex align-center">
+              <img
+                className="mr-20"
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="mr-20 d-flex justify-between">
+                <div className="box">
+                  <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                  <b>12 999 руб.</b>
+                </div>
+                <img
+                  className="ml-40 removeBtn"
+                  src="/img/btn-remove.svg"
+                  alt="Remove"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+          <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            </div>
+             <button className="greenButton">Оформить заказ <img src="/img/arrow.svg" alt="arrow" /></button>
+        </div>
+      </div>
+        
+     );
+}
+ 
+export default Drawer;
